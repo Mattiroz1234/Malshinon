c3568a404597195e2fb31bda5431fab3b5d00ba4
matti <m0556764778@gmail.com>
2025-06-09 15:06:20 +0300

    add class person

---
+++ Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Malshinon
+{
+    internal class Person
+    {
+        internal int Id;
+        internal string FirstName;
+        internal string LastName;
+        internal string SecretCode;
+        internal string Tipe;
+        internal int NumReports;
+        internal int NumMentions;
+
+        public Person(int id, string firstName, string lastName, string secretCode, string tipe, int numReports, int numMentions)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            SecretCode = secretCode;
+            Tipe = tipe;
+            NumReports = numReports;
+            NumMentions = numMentions;
+        }
+    }
+}

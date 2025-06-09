a7235365e505ae007a794123163d33c4f5f6588e
matti <m0556764778@gmail.com>
2025-06-09 12:57:28 +0300

    Add project files.

---
+++ SqlQuery_1.sql
@@ -0,0 +1,23 @@
+﻿CREATE DATABASE IF NOT EXISTS Malshinon; 
+
+USE Malshinon;
+
+CREATE TABLE IF NOT EXISTS People(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(32) NOT NULL,
+    last_name VARCHAR(32) NOT NULL,
+    secret_code VARCHAR(5) UNIQUE NOT NULL,
+    type ENUM('reporter', 'target', 'both', 'potential_agent') NOT NULL, 
+    num_reports INT(3) DEFAULT 0,
+    num_mentions INT(3) DEFAULT 0
+);
+
+CREATE TABLE IF NOT EXISTS IntelReports(
+	id INT AUTO_INCREMENT PRIMARY KEY, 
+    reporter_id INT NOT NULL,
+    target_id INT NOT NULL,
+    text TEXT(256) NOT NULL,
+    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY(reporter_id) REFERENCES People(id),
+    FOREIGN KEY(target_id) REFERENCES People(id)
+); 
